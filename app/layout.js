2249f6e15f43d572f9f1803d42da6bd96c56e306
logisoltech <operations@logisol.tech>
2026-04-21 12:42:58 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -39,6 +39,19 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-ZL59YZVHCG');
           `}
         </Script>
+        <Script id="tawk-to-script" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function() {
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = "https://embed.tawk.to/69e6bdffe5e4331c2fd0b889/1jmmlcc9o";
+              s1.charset = "UTF-8";
+              s1.setAttribute("crossorigin", "*");
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
         {children}
       </body>
     </html>
